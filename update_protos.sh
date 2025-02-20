b9b26c9f64086d178c1af62b351fcefa00f1b22b
deepak-saini <deepak1.kumar@stashfin.com>
2025-02-20 13:47:16 +0530

    added log in notification

--- update_protos.sh
+++ update_protos.sh
@@ -2,14 +2,15 @@
 
 # Define the base Go module path
 BASE_PACKAGE="github.com/stashfin2/grpc/stashfin.com"
+MAIN_PACKAGE="stashfin.com"
 
 # Define the proto directory
 PROTO_DIR="protos"
 
 # Generate Go options dynamically for all .proto files
 GO_OPTS=""
-for proto_file in $(find $PROTO_DIR -name "*.proto"); do
-    proto_file_rel="${proto_file#"$PROTO_DIR/"}"  # Remove leading "proto/"
+for proto_file in $(find "$PROTO_DIR" -name "*.proto"); do
+    proto_file_rel="${proto_file#"$PROTO_DIR/"}"  # Remove leading "protos/"
 
     # Extract directory path (excluding filename)
     proto_dir=$(dirname "$proto_file_rel")
@@ -28,9 +29,14 @@ done
 
 # Run the protoc command
 protoc \
-  --proto_path=$PROTO_DIR \
+  --proto_path="$PROTO_DIR" \
   --go_out=. --go-grpc_out=. \
   $GO_OPTS \
-  $(find $PROTO_DIR -name "*.proto")
+  $(find "$PROTO_DIR" -name "*.proto")
+
+# Move generated files to the correct location
+mkdir -p "$MAIN_PACKAGE"
+mv grpc/github.com/stashfin2/grpc/stashfin.com/* "$MAIN_PACKAGE/" 2>/dev/null || true
+rm -rf grpc/github.com
 
 echo "✅ Protobuf files successfully generated!"
